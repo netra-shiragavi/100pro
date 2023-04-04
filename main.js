9ce88f7e2f8b2769840dcf4aea0821997098e09d
netra-shiragavi <82329541+netra-shiragavi@users.noreply.github.com>
2023-04-04 17:35:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition() ;
+
+
+function on(){
+    recognition.start();
+
+}
+recognition.onresult = function(event){
+    console.log(event);
+    var holder = event.results[0][0].transcript;
+    if (holder == "selfie"){
+        speak();
+    }
+   
+   
+}
+
+
+function speak(){
+    var synth = speechSynthesis;
+   
+    Webcam.attach(camera);
+    setTimeout(function(){
+        img_id="selfie1";
+        take_snapshot();
+        speak_data = "Taking your selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(event.results[0][0].transcript);
+        synth.speak(utterThis);
+    
+        
+    },5000);   
+    
+    setTimeout(function(){
+        img_id="selfie2";
+        take_snapshot();
+        speak_data = "Taking your selfie in 10 seconds";
+        var utterThis = new SpeechSynthesisUtterance(event.results[0][0].transcript);
+        synth.speak(utterThis);
+    
+      
+    },10000);   
+    
+    setTimeout(function(){
+        img_id="selfie3";
+        take_snapshot();
+        speak_data = "Taking your selfie in 15 seconds";
+        var utterThis = new SpeechSynthesisUtterance(event.results[0][0].transcript);
+        synth.speak(utterThis);
+    
+       
+    },15000);
+}
+
+Window.set({
+    width: 360,
+    height: 360,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        if(img_id == "selfie1"){
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'">';
+        }
+
+        if(img_id == "selfie2"){
+            document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data_uri+'">';
+        }
+
+        if(img_id == "selfie3"){
+            document.getElementById("result3").innerHTML = '<img id="selfie3" src="'+data_uri+'">';
+        }
+
+    });
+}
+
